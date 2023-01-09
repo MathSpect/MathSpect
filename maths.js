55832d2e909b1424fe4e1145766ab1cb86effb93
Int Fract <110207136+intfract@users.noreply.github.com>
2023-01-10 08:30:27 +1300

    deleted redundant files

--- maths.js
+++
@@ -1,7 +0,0 @@
-const getRandomNumber = require('./functions/randomNumber');
-const range = require('./functions/range');
-const isPrime = require('./functions/isPrime');
-
-// console.log(getRandomNumber(0, 3, true));
-// console.log(range(0, 40));
-console.log(isPrime(13));
