024534e2e2120c2db94388585fd3b69fc944d580
TheBugCoder <67200019+TheBugCoder@users.noreply.github.com>
2023-01-09 20:32:16 +0100

    Create isPrime.ts

---
+++ src/functions/isPrime.ts
@@ -0,0 +1,15 @@
+function isPrime(number) {
+  if (number < 2 || (number > 2 && number % 2 === 0)) {
+    return false;
+  }
+
+  for (let i = 3; i <= Math.sqrt(number); i += 2) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+                                        
+export isPrime;
