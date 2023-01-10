203d4ba5fe97683ac27fa551605a6db9f97d9023
TheBugCoder <67200019+TheBugCoder@users.noreply.github.com>
2023-01-10 07:54:04 +0100

    Update random.ts

--- src/functions/random.ts
+++ src/functions/random.ts
@@ -1,6 +1,6 @@
 // Get a random number between a minimum and a maximum value
 
-export function getRandomNumber(min: number, max: number, rounded: boolean) {
+export function randomNumber(min: number, max: number, rounded: boolean) {
   const formula = Math.random() * (max - min) + min;
   return rounded ? Math.round(formula) : formula;
 }
